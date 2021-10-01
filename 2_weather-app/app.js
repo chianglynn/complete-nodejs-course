9155a92dd18321c0924fd0dd41c1c12c075d1529
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-01 20:38:28 +0800

    Chain callbacks

--- 2_weather-app/app.js
+++ 2_weather-app/app.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Tokyo', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+const location = process.argv[2];
 
-forecast(37.8267, -122.4233, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
+if (!location) return console.log('Please enter a location.');
+
+geocode(location, (error, geoData) => {
+    if (error) return console.log('Error', error);
+
+    forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+        if (error) return console.log('Error', error);
+
+        console.log(geoData.location);
+        console.log(forecastData);
+    });
 });
