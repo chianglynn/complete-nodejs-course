eb4b1a4181225bb875152219cca71c0b0a416be9
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-01 17:58:20 +0800

    Refactor mapbox API HTTP request  to a callback function

--- 2_weather-app/app.js
+++ 2_weather-app/app.js
@@ -1,36 +1,24 @@
+require('dotenv').config();
 const request = require('postman-request');
-
-// mapbox API
-// document: https://docs.mapbox.com/api/search/geocoding/
-const ACCESS_TOKENS = '';
-const MAPBOX_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${ACCESS_TOKENS}&limit=1`;
-
-request({ url: MAPBOX_URL, json: true }, (error, response) => {
-    if (error) {
-        console.log(`Unable to connect location service`);
-    } else if (response.body.message || !response.body.features[0]) {
-        console.log(`Unable to find location.`);
-    } else {
-        const { place_name, center } = response.body.features[0];
-        const [longitude, latitude] = center;
-        console.log(`latitude: ${latitude}, longitude: ${longitude}`);
-    }
-});
+const geocode = require('./utils/geocode');
 
 // Weather Stack API
 // document: https://weatherstack.com/documentation
-const ACCESS_KEY = '';
-const LATITUDE_LONGITUDE = '37.8267,-122.4233';
-const WEATHER_STACK_URL = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${LATITUDE_LONGITUDE}&units=m`;
+// const WEATHER_STACK_URL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_ACCESS_KEY}&query=${LATITUDE_LONGITUDE}&units=m`;
 
-request({ url: WEATHER_STACK_URL, json: true }, (error, response) => {
-    if (error) {
-        console.log(`Unable to connect weather service`);
-    } else if (response.body.error) {
-        console.log(`Unable to find location. ${response.body.error.info}`);
-    } else {
-        const data = response.body.current;
-        const { temperature, weather_descriptions, feelslike } = data;
-        console.log(`${weather_descriptions}. It's currently ${temperature} degrees. It feels like ${feelslike} degrees`);
-    }
+// request({ url: WEATHER_STACK_URL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log(`Unable to connect weather service`);
+//     } else if (response.body.error) {
+//         console.log(`Unable to find location. ${response.body.error.info}`);
+//     } else {
+//         const data = response.body.current;
+//         const { temperature, weather_descriptions, feelslike } = data;
+//         console.log(`${weather_descriptions}. It's currently ${temperature} degrees. It feels like ${feelslike} degrees`);
+//     }
+// });
+
+geocode('Tokyo', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
 });
---
+++ 2_weather-app/utils/geocode.js
@@ -0,0 +1,20 @@
+const request = require('postman-request');
+
+// mapbox API
+// document: https://docs.mapbox.com/api/search/geocoding/
+const geocode = (address, callback) => {
+    const MAPBOX_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/encodeURIComponent(${address}).json?access_token=${process.env.MAPBOX_ACCESS_TOKENS}&limit=1`;
+    request({ url: MAPBOX_URL, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect location service', undefined);
+        } else if (response.body.message || !response.body.features[0]) {
+            callback('Unable to find location.', undefined);
+        } else {
+            const { place_name, center } = response.body.features[0];
+            const [longitude, latitude] = center;
+            callback(undefined, { location: place_name, latitude, longitude });
+        }
+    });
+};
+
+module.exports = geocode;
