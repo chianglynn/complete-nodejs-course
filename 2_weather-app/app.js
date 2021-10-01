5fd5b465a13734b8ec97254fe25aa4cc3934b278
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-01 19:45:59 +0800

    Refactor weather stack API HTTP request to a callback function

--- 2_weather-app/app.js
+++ 2_weather-app/app.js
@@ -1,24 +1,13 @@
 require('dotenv').config();
-const request = require('postman-request');
 const geocode = require('./utils/geocode');
-
-// Weather Stack API
-// document: https://weatherstack.com/documentation
-// const WEATHER_STACK_URL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_ACCESS_KEY}&query=${LATITUDE_LONGITUDE}&units=m`;
-
-// request({ url: WEATHER_STACK_URL, json: true }, (error, response) => {
-//     if (error) {
-//         console.log(`Unable to connect weather service`);
-//     } else if (response.body.error) {
-//         console.log(`Unable to find location. ${response.body.error.info}`);
-//     } else {
-//         const data = response.body.current;
-//         const { temperature, weather_descriptions, feelslike } = data;
-//         console.log(`${weather_descriptions}. It's currently ${temperature} degrees. It feels like ${feelslike} degrees`);
-//     }
-// });
+const forecast = require('./utils/forecast');
 
 geocode('Tokyo', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+});
+
+forecast(37.8267, -122.4233, (error, data) => {
     console.log('Error', error);
     console.log('Data', data);
 });
---
+++ 2_weather-app/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    // Weather Stack API
+    // document: https://weatherstack.com/documentation
+    const WEATHER_STACK_URL = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_STACK_ACCESS_KEY}&query=${latitude},${longitude}&units=m`;
+
+    request({ url: WEATHER_STACK_URL, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect weather service.', undefined);
+        } else if (response.body.error) {
+            callback(`Unable to find location. ${response.body.error.info}`, undefined);
+        } else {
+            const data = response.body.current;
+            const { temperature, weather_descriptions, feelslike } = data;
+            callback(undefined, `${weather_descriptions}. It's currently ${temperature} degrees. It feels like ${feelslike} degrees.`);
+        }
+    });
+};
+
+module.exports = forecast;
