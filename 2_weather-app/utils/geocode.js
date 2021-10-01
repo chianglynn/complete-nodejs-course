fbde94087ff7d079eb20fb188b54546841ad4d97
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-01 19:27:33 +0800

    Update geocode.js

--- 2_weather-app/utils/geocode.js
+++ 2_weather-app/utils/geocode.js
@@ -4,6 +4,7 @@ const request = require('postman-request');
 // document: https://docs.mapbox.com/api/search/geocoding/
 const geocode = (address, callback) => {
     const MAPBOX_URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/encodeURIComponent(${address}).json?access_token=${process.env.MAPBOX_ACCESS_TOKENS}&limit=1`;
+
     request({ url: MAPBOX_URL, json: true }, (error, response) => {
         if (error) {
             callback('Unable to connect location service', undefined);
