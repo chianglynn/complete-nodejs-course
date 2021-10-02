c672ee1132a94ed27ef3ab00e2449ce3ee2eeedb
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-02 11:32:14 +0800

    Replace index.html with index.hbs

--- 2_web-server/src/app.js
+++ 2_web-server/src/app.js
@@ -4,9 +4,12 @@ const express = require('express');
 const publicDirectoryPath = path.join(__dirname, '../public');
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
-
+app.get('', (request, response) => {
+    response.render('index');
+});
 
 app.get('/weather', (request, response) => {
     response.send('Weather page');
