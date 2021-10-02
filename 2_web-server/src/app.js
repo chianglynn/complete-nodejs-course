2b0dc890ce6e6d503c46b291f186977c7c75a698
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-02 11:40:51 +0800

    Replace html files with hbs files

--- 2_web-server/src/app.js
+++ 2_web-server/src/app.js
@@ -8,7 +8,22 @@ app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (request, response) => {
-    response.render('index');
+    response.render('index', {
+        title: 'Weather',
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about', {
+        title: 'About',
+    });
+});
+
+app.get('/help', (request, response) => {
+    response.render('help', {
+        title: 'Help',
+        text: 'Help text',
+    });
 });
 
 app.get('/weather', (request, response) => {
