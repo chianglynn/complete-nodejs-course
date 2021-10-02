c2cb6882a01de0ba783b74e8b224aaabe3d42cd5
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-02 18:42:45 +0800

    Finish route and styling

--- 2_web-server/src/app.js
+++ 2_web-server/src/app.js
@@ -1,33 +1,62 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
-const publicDirectoryPath = path.join(__dirname, '../public');
 const app = express();
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Set up handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Set up directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (request, response) => {
     response.render('index', {
         title: 'Weather',
+        name: 'Someone',
     });
 });
 
 app.get('/about', (request, response) => {
     response.render('about', {
         title: 'About',
+        name: 'Someone',
     });
 });
 
 app.get('/help', (request, response) => {
     response.render('help', {
         title: 'Help',
+        name: 'Someone',
         text: 'Help text',
     });
 });
 
 app.get('/weather', (request, response) => {
     response.send('Weather page');
+});
+
+app.get('/help/*', (request, response) => {
+    response.render('404', {
+        title: '404',
+        message: 'Help aticle not found.',
+        name: 'Someone',
+    });
+});
+
+app.get('*', (request, response) => {
+    response.render('404', {
+        title: '404',
+        message: 'Page not found.',
+        name: 'Someone',
+    });
 });
 
 app.listen(3000, () => console.log('Server is up on port 3000'));
