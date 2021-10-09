3ecfb19f3ae582239be6204f4e294679ea1e8f92
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-09 14:33:36 +0800

    Generate authentication tokens

--- 3_task-app/src/models/user.js
+++ 3_task-app/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -10,6 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -33,8 +35,38 @@ const userSchema = new mongoose.Schema({
             if (value < 0) throw new Error('Age must be a positive number.');
         }
     },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }],
 });
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'newcourse');
+
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+
+    return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) throw new Error('Unable to login.');
+
+    const isMatched = await bcrypt.compare(password, user.password);
+
+    if (!isMatched) throw new Error('Unable to login.');
+
+    return user;
+};
+
+
+// Hash plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this;
 
