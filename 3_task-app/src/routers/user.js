fc899a4238d119ac43db0ad3ac6ce9c353178918
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-20 23:46:08 +0800

    Finish upload/delete user avatar

--- 3_task-app/src/routers/user.js
+++ 3_task-app/src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
@@ -60,7 +61,8 @@ router.post('/users/logoutAll', auth, async (req, res) => {
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
@@ -69,6 +71,17 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) 
 
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
+});
+
+router.get('/users/:id/avatar', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user || !user.avatar) throw new Error();
+        res.set('Content-type', 'image/png').send(user.avatar);
+    } catch (error) {
+        res.status(404).send();
+    }
 });
 
 router.patch('/users/me', auth, async (req, res) => {
