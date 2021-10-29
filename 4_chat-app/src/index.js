9545cfce8448f3ed21902a207bfa5817b682c73b
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-29 15:28:15 +0800

    Add broadcast event

--- 4_chat-app/src/index.js
+++ 4_chat-app/src/index.js
@@ -15,8 +15,11 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log('socket.io');
 
-    socket.emit('message', 'Welcome');
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined.');
+
     socket.on('sendMessage', message => io.emit('message', message));
+    socket.on('disconnect', () => io.emit('message', 'A user has left.'));
 });
 
 server.listen(port, () => console.log(`Server is up on port ${port}.`));
