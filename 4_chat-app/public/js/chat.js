7954e6872c99592bf8df259b70fa78371edd96db
Chiang Lynn <lynnchiangtw@gmail.com>
2021-10-30 16:23:31 +0800

    Add/remove disable attribute of buttons

--- 4_chat-app/public/js/chat.js
+++ 4_chat-app/public/js/chat.js
@@ -1,7 +1,8 @@
 const socket = io();
 const messageForm = document.getElementById('message-form');
 const messageInput = messageForm.querySelector('input');
-const sendLocation = document.getElementById('send-location');
+const messageButton = messageForm.querySelector('button');
+const sendLocationButton = document.getElementById('send-location');
 
 
 socket.on('message', message => console.log(message));
@@ -9,20 +10,32 @@ socket.on('message', message => console.log(message));
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    // Disable form button
+    messageButton.setAttribute('disabled', 'disabled');
+
     const message = messageInput.value;
+
     socket.emit('sendMessage', message, (error) => {
+        // Enable form button
+        messageButton.removeAttribute('disabled');
+        messageInput.value = '';
+        messageInput.focus();
+
         if (error) console.log(error);
         console.log('Message was delivered.');
     });
 });
-sendLocation.addEventListener('click', () => {
+sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) return alert('Geolocation is not supported by your browser.');
 
+    sendLocationButton.setAttribute('disabled', 'disabled');
+
     navigator.geolocation.getCurrentPosition(position => {
-        sendLocation.emit('sendLocation', {
+        sendLocationButton.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         }, () => {
+            sendLocationButton.removeAttribute('disabled');
             console.log('Location shared.');
         });
     });
